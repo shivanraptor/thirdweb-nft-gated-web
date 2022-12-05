2cafde8c7ffffd0168772b39a58117d8a5e7cb01
shivanraptor <raptor.shivan@gmail.com>
2022-12-05 21:21:21 +0800

    Initial commit

---
+++ const/yourDetails.js
@@ -0,0 +1,5 @@
+// Replace this with your contract address
+export const contractAddress = "0xAE65da49E0E6c0EF9A0F36BdBb06895Aa0250BFa";
+
+// Replace this with your domain name.
+export const domainName = "locahost";
